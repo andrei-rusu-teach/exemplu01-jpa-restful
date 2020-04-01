3ac337effc00639ca817b18528706a389aaac85b
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-04-01 10:20:15 +0300

    H2, adaugate date in owner, si car : legatura N:1 intre car-owner

--- src/main/java/eu/andreirusu/daw/cardb/Daw07aApplication.java
+++ src/main/java/eu/andreirusu/daw/cardb/Daw07aApplication.java
@@ -10,12 +10,17 @@ import org.springframework.context.annotation.Bean;
 
 import eu.andreirusu.daw.cardb.domain.Car;
 import eu.andreirusu.daw.cardb.domain.CarRepository;
+import eu.andreirusu.daw.cardb.domain.Owner;
+import eu.andreirusu.daw.cardb.domain.OwnerRepository;
 
 @SpringBootApplication
 public class Daw07aApplication {
 
 	@Autowired
 	private CarRepository carRepository;
+
+	@Autowired
+	private OwnerRepository ownerRepository;
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(Daw07aApplication.class);
@@ -32,12 +37,19 @@ public class Daw07aApplication {
 			logger
 					.info("\n... Application started to INSERT VALUES into DB :) ...\n");
 			// Save demo data to database
-			carRepository
-					.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
-			carRepository
-					.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
-			carRepository
-					.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+
+			// Add owner objects and save these to db
+			Owner owner1 = new Owner("John", "Johnson");
+			Owner owner2 = new Owner("Mary", "Robinson");
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
+
+			carRepository.save(
+					new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
+			carRepository.save(
+					new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
+			carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018,
+					39000, owner2));
 		};
 	}
 }
