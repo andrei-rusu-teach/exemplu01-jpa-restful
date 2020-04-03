0c88a3b32dde6f8fdb474b8167f5a58ef953f418
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-04-03 18:07:14 +0300

    Adaugat utilizator in-memory, dar asta anuleaza regulile de securitate implicite

--- src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
+++ src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
@@ -1,9 +1,14 @@
 package eu.andreirusu.daw.cardb;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,11 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	}
 
-//	@Bean
-//	@Override
-//	public UserDetailsService userDetailsService() {
-//		UserDetails user = User.withDefaultPasswordEncoder().username("user")
-//				.password("password").roles("USER").build();
-//		return new InMemoryUserDetailsManager(user);
-//	}
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user")
+				.password("password").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
+	}
 }
