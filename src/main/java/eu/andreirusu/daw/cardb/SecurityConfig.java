c75b367f4a2c094621cb817258400c04a924c907
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-04-04 11:31:03 +0300

    Finalizata configurarea Basic Authentication cu utilzatori definiti in BD

--- src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
+++ src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
@@ -3,7 +3,6 @@ package eu.andreirusu.daw.cardb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,9 +15,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailServiceImpl userDetailsService;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	}
+	// Metoda responsabila de stabilirea restictiilor de acces la resurse
+	// Daca nu are nimic (i.e. este pustie) implicit este permis orice
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
