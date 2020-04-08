e360eb3fad848cc1b6eb3e39486527a4fb638e67
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-04-08 10:42:44 +0300

    In SecurityCOnfig adaugat @//formatter:off/on

--- src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
+++ src/main/java/eu/andreirusu/daw/cardb/SecurityConfig.java
@@ -32,15 +32,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login")
-				.permitAll().anyRequest().authenticated().and()
+	protected void configure(HttpSecurity http) throws Exception { // @formatter:off
+		http
+			.cors()
+			.and()
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/login")
+					.permitAll()
+				.anyRequest()
+					.authenticated()
+			.and()
 				// Filter for the api/login requests
 				.addFilterBefore(new LoginFilter("/login", authenticationManager()),
 						UsernamePasswordAuthenticationFilter.class)
 				// Filter for other requests to check JWT in header
 				.addFilterBefore(new AuthenticationFilter(),
-						UsernamePasswordAuthenticationFilter.class);
+						UsernamePasswordAuthenticationFilter.class); // @formatter:on
 	}
 
 	@Bean
